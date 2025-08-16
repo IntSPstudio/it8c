87c0f2e8a92647cf306fe3476116968ed4232927
IntSPstudio <official.intsp@gmail.com>
2025-08-17 01:04:45 +0300

    1025.031708.9800.1.4

--- useful-scripts/csvconverter.py
+++ useful-scripts/csvconverter.py
@@ -2,28 +2,27 @@
 # Made by IntSPstudio
 # Useful scripts to IT8c library
 # Thank you for using this software!
-# Version: 0.0.1.20170412
+# Version: 0.0.2.20250816
 # ID: 980001004
-#
-# Twitter: @IntSPstudio
 #|==============================================================|#
 
 #IMPORT
 import it8c
 #START
 if __name__ == "__main__":
-	print(it8c.vslTerminalLine(0,""))
+	print(it8c.vsl_tui_logo(1))
+	print(it8c.vsl_tui_line(0,""))
 	checka = str(input("Filename: "))
-	if it8c.fileTextExists(checka) == 1:
+	if it8c.file_exists(checka) == 1:
 		checkb = str(input("Text separator: "))
 		if checkb !="":
 			checkc = str(input("Save file as: "))
 			if checkc !="":
 				checkd = str(input("New separator: ") or ";")
 				if checkd !="":
-					contentArray = it8c.csvReadFile(checka,checkb)
-					it8c.csvWriteFile(contentArray,checkc,checkd,0)
-					contentArray =0
+					content = it8c.file_read_csv(checka,checkb)
+					it8c.file_write_csv(content,checkc,checkd,0)
+					content =0
 				else:
 					print("Invalid separator")
 			else:
